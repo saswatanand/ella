fa3057db66877b5bee153c6c564bd331fcc6a319
saswat Anand <saswat@apac1.stanford.edu>
2014-12-16 17:38:47 -0800

    added servlet to view coverage data

---
+++ frontend/src/com/apposcopy/ella/frontend/ViewCoverage.java
@@ -0,0 +1,113 @@
+package com.apposcopy.ella.frontend;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+
+@WebServlet(name = "ViewCoverage", urlPatterns = {"/viewcoverage"})
+public class ViewCoverage extends HttpServlet
+{
+	private static Logger logger = Logger.getLogger(ViewCoverage.class.getName());
+    private static final int DEFAULT_BUFFER_SIZE = 10240; // 10KB.                                                                     
+    public ViewCoverage() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request,
+						 HttpServletResponse response)
+		throws ServletException, IOException 
+	{
+		response.setContentType("text/html;charset=UTF-8");
+
+		final PrintWriter writer = response.getWriter();
+
+		// Create path components to save the file
+		String appPath = request.getParameter("apppath");
+		String ellaOutDir = getServletContext().getInitParameter("ella.outdir");
+
+		File appFile = new File(appPath);
+		if(!appFile.exists()){
+			writer.println("File "+appPath+" does not exist.");
+			return;
+		}
+
+		String appId = appFile.getCanonicalPath().replace(File.separatorChar, '_');
+
+		//System.out.println("pkg: "+pkg+" covData: "+covData+" ell.dir: "+ellaDir);
+
+		BufferedReader covReader = null;
+		try {
+			String path = ellaOutDir + File.separator + appId;
+			generateReport(writer, path);
+			/*
+			File dir = new File(path);
+			covReader = new BufferedReader(new FileReader(new File(dir, "coverage.dat")));
+			String covData = covReader.readLine();
+			writer.println(covData);
+			*/
+			logger.log(Level.INFO, "ViewCoverage done.");
+		} catch (FileNotFoundException fne) {
+			writer.println("You either did not specify a file to upload or are "
+                + "trying to upload a file to a protected or nonexistent "
+						   + "location.");
+			writer.println("<br/> ERROR: " + fne.getMessage());
+
+			logger.log(Level.SEVERE, "Problems during file upload. Error: {0}", 
+					   new Object[]{fne.getMessage()});
+		} finally {
+			if (covReader != null) {
+				covReader.close();
+			}
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+	void generateReport(PrintWriter writer, String ellaOutDir) throws IOException
+	{
+		String idPath = ellaOutDir + File.separator + "covids";
+		BufferedReader idReader = new BufferedReader(new FileReader(idPath));
+		List<String> meths = new ArrayList();
+		String line;
+		while((line = idReader.readLine()) != null){
+			meths.add(line);
+			//System.out.println(line);
+		}
+		idReader.close();
+
+		String covPath = ellaOutDir + File.separator + "coverage.dat";
+		BufferedReader covReader = new BufferedReader(new FileReader(covPath));
+		String covData = covReader.readLine();
+		covData = covData.substring(1,covData.length()-1); //drop the leading { and trailing }
+		String[] ids = covData.split(", ");
+
+		//writer.print("<![CDATA[");
+		writer.print("<ul>");
+		for(String id : ids){
+			int i = Integer.parseInt(id);
+			String m = meths.get(i);
+			writer.println("<li>"+escapeHtml4(m)+"</li>");
+			System.out.println(i + " " + m);
+		}
+		writer.print("</ul>");
+		//writer.print("]]>");
+		covReader.close();
+	}
+
+}
